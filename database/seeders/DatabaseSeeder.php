9c1acec52c30ad07a996f81f3cf837a962d117b3
sirjoaogoncalves <joaogon85@gmail.com>
2024-04-22 16:28:28 +0100

    yayaya startup

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Project;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        User::factory()->create([
+             'name' => 'Joao',
+             'email' => 'joao@example.com',
+             'password' => bcrypt('caldelas123'),
+
+         ]);
+
+        Project::factory()
+            ->count(10)
+            ->hasTasks(10)
+            ->create();
+
+    }
+}
